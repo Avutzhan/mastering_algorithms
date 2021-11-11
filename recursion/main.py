ee0db86721d58ef2d1765fffd60971190e7b539a
avutzhan <dautov92@list.ru>
2021-11-11 20:22:53 +0600

    gcd

--- recursion/main.py
+++ recursion/main.py
@@ -2,3 +2,48 @@ def factorial(num: int):
     if num == 0:
         return 1
     return factorial(num - 1) * num
+
+
+def gcd(a: int, b: int) -> int:
+    """
+    greatest common divisor
+    euclid's algorithm
+    НОД наибольший общий делитель двух чисел
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    if a == b:
+        return a
+    elif a > b:
+        return gcd(a - b, b)
+    else:  # a < b
+        return gcd(a, b - a)
+
+
+def gcd_better(a: int, b: int) -> int:
+    """
+    greatest common divisor
+    euclid's algorithm
+    НОД наибольший общий делитель двух чисел
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+def gcd_one_liner(a: int, b: int) -> int:
+    """
+    greatest common divisor
+    euclid's algorithm
+    НОД наибольший общий делитель двух чисел
+    :param a: int
+    :param b: int
+    :return: int
+    """
+
+    return a if b == 0 else gcd(b, a % b)
--- tests/recursion/test_recursion_main.py
+++ tests/recursion/test_recursion_main.py
@@ -10,12 +10,57 @@ class TestRecursion(unittest.TestCase):
     ])
     def test_factorial(self, name, data, expected):
         """
-        Test that it can insert sort array
+        Test that it can factorial number
         :param name: string
-        :param data: list
-        :param expected: list
+        :param data: int
+        :param expected: int
         """
         result = recursion.main.factorial(data)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ["test_1", 5, 10, 5],
+        ["test_2", 6, 12, 6]
+    ])
+    def test_gcd(self, name, a, b, expected):
+        """
+        Test that it can find gcd
+        :param name: string
+        :param a: int
+        :param b: int
+        :param expected: int
+        """
+        result = recursion.main.gcd(a, b)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ["test_1", 5, 10, 5],
+        ["test_2", 6, 12, 6]
+    ])
+    def test_gcd_better(self, name, a, b, expected):
+        """
+        Test that it can find gcd
+        :param name: string
+        :param a: int
+        :param b: int
+        :param expected: int
+        """
+        result = recursion.main.gcd_better(a, b)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ["test_1", 5, 10, 5],
+        ["test_2", 6, 12, 6]
+    ])
+    def test_gcd_one_liner(self, name, a, b, expected):
+        """
+        Test that it can find gcd
+        :param name: string
+        :param a: int
+        :param b: int
+        :param expected: int
+        """
+        result = recursion.main.gcd_one_liner(a, b)
         self.assertEqual(result, expected)
 
 
